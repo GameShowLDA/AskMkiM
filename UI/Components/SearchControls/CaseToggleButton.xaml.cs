000105c27907302cafa5a1f20a360b3c249f5938
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2025-02-03 14:45:34 +0400

    Новые элемента

---
+++ UI/Components/SearchControls/CaseToggleButton.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using YamlDotNet.Core.Tokens;
+
+namespace UI.Components.SearchControls
+{
+  /// <summary>
+  /// Логика взаимодействия для CaseToggleButton.xaml
+  /// </summary>
+  public partial class CaseToggleButton : UserControl
+  {
+
+
+    public CaseToggleButton()
+    {
+      InitializeComponent();
+    }
+
+    public bool IsChecked { get => GetChecked(); set => SetChecked(value); }
+
+    private void SetChecked(bool value)
+    {
+      if (value)
+      {
+        ToggleButton.Foreground = Brushes.Red;
+      }
+      else
+      {
+        ToggleButton.Foreground = Brushes.Black;
+      }
+    }
+
+    private bool GetChecked()
+    {
+      if (ToggleButton.Foreground == Brushes.Red)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+    private void ToggleButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+    {
+      IsChecked = !IsChecked;
+    }
+  }
+}
--- UI/Controls/Search/SearchWindow.xaml.cs
+++ UI/Controls/Search/SearchWindow.xaml.cs
@@ -1,14 +1,16 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using UI.Components.SearchControls;
 
 namespace UI.Controls.Search
 {
   public partial class SearchWindow : Window
   {
-    private bool _isExpanded = false; // Отслеживаем состояние
+    private bool _isExpanded = false; // По умолчанию строка скрыта
 
-    private const double MinWindowHeight = 100; // Высота без строки 2
-    private const double ExpandedWindowHeight = 140; // Высота со строкой 2
+    private const double MinWindowHeight = 80;  // Высота окна без строки 2
+    private const double ExpandedWindowHeight = 120; // Высота окна со строкой 2
 
     public SearchWindow()
     {
@@ -17,7 +19,42 @@ namespace UI.Controls.Search
 
     private void Window_Loaded(object sender, RoutedEventArgs e)
     {
+      ReplaceRow.Height = new GridLength(0);
+      UpdateWindowHeight(MinWindowHeight);
+    }
 
+    private async void ToggleArrow_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+    {
+      _isExpanded = !_isExpanded;
+
+
+      ToggleArrow.IsArrowUp = !_isExpanded;
+
+      await Task.Delay(250);
+      ReplaceRow.Height = _isExpanded ? new GridLength(1, GridUnitType.Star) : new GridLength(0);
+
+      UpdateWindowHeight(_isExpanded ? ExpandedWindowHeight : MinWindowHeight);
+    }
+
+    private void UpdateWindowHeight(double newHeight)
+    {
+      this.Height = newHeight;
+      this.MinHeight = newHeight;
+      this.MaxHeight = newHeight;
+    }
+
+    private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+      this.Height = this.MinHeight;
+    }
+
+    private void OnCaseChanged(object sender, EventArgs e)
+    {
+      var button = sender as CaseToggleButton;
+      if (button != null)
+      {
+        MessageBox.Show($"Кнопка включена: {button.IsChecked}");
+      }
     }
 
   }
