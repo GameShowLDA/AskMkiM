afe1fba4afbe7deb4c7cbfa766decbdf000633ba
Daniil Lukashin <vanafomin7602@gmail.com>
2025-03-04 16:25:44 +0400

    Пока добавляю только свою папку ServicesTest с пустыми классами и UserControl! Экономия времени при разработке в будущем!

---
+++ Mode/ServicesTest/MESH/MeshControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mode.ServerTest.MESH
+{
+  /// <summary>
+  /// Логика взаимодействия для MeshControl.xaml
+  /// </summary>
+  public partial class MeshControl : UserControl
+  {
+    public MeshControl()
+    {
+      InitializeComponent();
+    }
+  }
+}
