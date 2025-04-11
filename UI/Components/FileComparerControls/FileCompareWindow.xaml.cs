db51fe43bb4f219b9a72882a45738380e7599f5f
JuliaZorina <96383396+JuliaZorina@users.noreply.github.com>
2025-04-11 09:36:34 +0400

    Создано окно для выбора файлов для сравнения

---
+++ UI/Components/FileComparerControls/FileCompareWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI.Components.FileComparerControls
+{
+    /// <summary>
+    /// Логика взаимодействия для FileCompareWindow.xaml
+    /// </summary>
+    public partial class FileCompareWindow : Window
+    {
+        public FileCompareWindow()
+        {
+            InitializeComponent();
+        }
+
+    private void CompareButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+    {
+
+    }
+  }
+}
