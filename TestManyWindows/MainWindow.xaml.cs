524ef9e9cf5d00768f07311e063c8eaab3eb3f0f
JuliaZorina <96383396+JuliaZorina@users.noreply.github.com>
2025-05-30 15:55:58 +0400

    добавлено открытие файлов

--- TestManyWindows/MainWindow.xaml.cs
+++ TestManyWindows/MainWindow.xaml.cs
@@ -1,12 +1,44 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using UI.Controls.TextEditor;
 
 namespace TestDocking
 {
   public partial class MainWindow : Window
   {
+    private int _documentCounter = 1;
+
     public MainWindow()
     {
       InitializeComponent();
     }
+
+    private void OpenFile_Click(object sender, RoutedEventArgs e)
+    {
+      var dialog = new OpenFileDialog
+      {
+        Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*"
+      };
+
+      if (dialog.ShowDialog() == true)
+      {
+        string filePath = dialog.FileName;
+        string fileContent = File.ReadAllText(filePath);
+
+        var textEditor = new TextEditorUI();
+        textEditor.Text = File.ReadAllText(filePath);
+
+        var dockItem = new DevZest.Windows.Docking.DockItem
+        {
+          Title = Path.GetFileName(filePath),
+          TabText = Path.GetFileName(filePath),
+          Content = textEditor
+        };
+
+        dockItem.Show(DockManager, DevZest.Windows.Docking.DockPosition.Document);
+      }
+    }
+
   }
 }
