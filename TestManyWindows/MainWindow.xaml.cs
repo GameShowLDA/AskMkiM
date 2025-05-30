8e6a91a87433a9f78c31fc0f9cecd527c8300a46
JuliaZorina <96383396+JuliaZorina@users.noreply.github.com>
2025-05-30 13:53:41 +0400

    Добавила новую библиотеку, начала настраивать стили

--- TestManyWindows/MainWindow.xaml.cs
+++ TestManyWindows/MainWindow.xaml.cs
@@ -1,38 +1,12 @@
 ﻿using System.Windows;
-using UI.Controls.TextEditor;
 
-namespace TestManyWindows
+namespace TestDocking
 {
   public partial class MainWindow : Window
   {
-    public DockingPanel DockingPanel { get; private set; }
-
     public MainWindow()
     {
       InitializeComponent();
-
-      DockingPanel = new DockingPanel();
-      Content = DockingPanel;
-
-      AddNewTab("Документ 1");
-      AddNewTab("Документ 2");
-    }
-
-    private void NewFile_Click(object sender, RoutedEventArgs e)
-    {
-      AddNewTab($"Документ {DockingPanel.TabCount + 1}");
-    }
-
-    private void AddNewTab(string title)
-    {
-      var editor = new TextEditorUI(); // Твой редактор
-      DockingPanel.AddTab(title, editor);
-    }
-
-    public void ReceiveTab(string title, UIElement content)
-    {
-      DockingPanel.AddTab(title, content);
     }
   }
-
 }
