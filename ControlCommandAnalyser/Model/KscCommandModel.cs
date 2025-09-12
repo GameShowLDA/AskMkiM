82eec5837d927779093caa28b213d6028eca2fa5
JuliaZorina <96383396+JuliaZorina@users.noreply.github.com>
2025-09-12 09:48:35 +0400

    в кц добавлена команда ок

--- ControlCommandAnalyser/Model/KscCommandModel.cs
+++ ControlCommandAnalyser/Model/KscCommandModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ControlCommandAnalyser.Model.Ok;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace ControlCommandAnalyser.Model
 {
   public class KscCommandModel : BaseCommandModel
   {
+    public OkCommandModel OkCommandModel { get; set; }
     public override string Mnemonic => "КЦ";
   }
 }
--- ControlCommandAnalyser/Parser/KSC/KscCommandParser.cs
+++ ControlCommandAnalyser/Parser/KSC/KscCommandParser.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using ControlCommandAnalyser.Model;
+using ControlCommandAnalyser.Model.Ok;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ControlCommandAnalyser.Model;
-using ControlCommandAnalyser.Model.Ok;
+using Utilities;
 
 namespace ControlCommandAnalyser.Parser.KSC
 {
@@ -14,11 +15,33 @@ namespace ControlCommandAnalyser.Parser.KSC
 
     public BaseCommandModel Parse(string commandNumber, string mnemonic, int numberLine, List<string> lines)
     {
+      LoggerUtility.LogInformation($"Начало парсинга команды: {commandNumber} {mnemonic}, строк: {lines?.Count ?? 0}");
+
+      var okCommandModel = new OkCommandModel();
+      var commandModel = CommandsModel.GetByMnemonic("ОК").Last();
+
+      if (commandModel == null)
+      {
+        throw new Exception("ОК не сущесвует...");
+      }
+      else
+      {
+        if (commandModel is OkCommandModel)
+        {
+          okCommandModel = commandModel as OkCommandModel;
+        }
+        else
+        {
+          throw new Exception("ОК не сущесвует...");
+        }
+      }
+
       var model = new KscCommandModel
       {
         CommandNumber = commandNumber,
         SourceLines = new List<string>(lines),
         StartLineNumber = numberLine,
+        OkCommandModel = okCommandModel,
       };
 
       return model;
