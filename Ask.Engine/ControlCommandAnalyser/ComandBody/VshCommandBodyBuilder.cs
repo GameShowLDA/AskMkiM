2b3f7d29364c817fb6d8cac1d80ce437efe4ad5f
JuliaZorina <96383396+JuliaZorina@users.noreply.github.com>
2026-01-14 16:25:51 +0400

    Добавила еще CommandBodyBuilder для ВШ

---
+++ Ask.Engine/ControlCommandAnalyser/ComandBody/VshCommandBodyBuilder.cs
@@ -0,0 +1,53 @@
+﻿using Ask.Engine.ControlCommandAnalyser.Model;
+using System.Text;
+using Ask.Core.Shared.Metadata.Enums.TranslationEnums;
+
+namespace Ask.Engine.ControlCommandAnalyser.ComandBody
+{
+  public class VshCommandBodyBuilder : ICommandBody
+  {
+    public bool CanCreate(BaseCommandModel model) => model is VshCommandModel;
+
+    public StringBuilder Create(BaseCommandModel model, StringBuilder newSourseLines)
+    {
+      if (model is not VshCommandModel vsh)
+      {
+        return newSourseLines;
+      }
+      var commandBody = new StringBuilder();
+      if (vsh.BusStructure.Count > 0)
+      {
+        var busNumber = string.Empty;
+        commandBody.Append($"*");
+        foreach (var item in vsh.BusStructure)
+        {
+          if(item.Key == BusStructureEnum.Type.Bus2)
+          {
+            busNumber = "2";
+          }
+          if(item.Key == BusStructureEnum.Type.Bus4)
+          {
+            busNumber = "4";
+          }
+          if(item.Key == BusStructureEnum.Type.Bus6)
+          {
+            busNumber = "6";
+          }
+          if(item.Key == BusStructureEnum.Type.Bus8)
+          {
+            busNumber = "8";
+          }
+          if(item.Key == BusStructureEnum.Type.BusCombined)
+          {
+            busNumber = "К";
+          }
+          foreach (var number in item.Value)
+          {
+            commandBody.Append($"{busNumber}Ш:{number}*");
+          }
+        }
+      }
+      return newSourseLines.Append(commandBody.ToString());
+    }
+  }
+}
