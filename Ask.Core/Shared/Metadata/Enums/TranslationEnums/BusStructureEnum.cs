cb5592daba6094fbbdd20fc7d118d226bb5a5be4
JuliaZorina <96383396+JuliaZorina@users.noreply.github.com>
2026-01-14 16:14:21 +0400

    ну вроде ВШ сделала

---
+++ Ask.Core/Shared/Metadata/Enums/TranslationEnums/BusStructureEnum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ask.Core.Shared.Metadata.Enums.TranslationEnums
+{
+  public class BusStructureEnum
+  {
+    public enum Type
+    {
+      /// <summary>
+      /// Двухшинная структура стойки коммутации.
+      /// </summary>
+      Bus2,
+
+      /// <summary>
+      /// Четырехшинная структура стойки коммутации.
+      /// </summary>
+      Bus4,
+
+      /// <summary>
+      /// Шестишинная структура стойки коммутации.
+      /// </summary>
+      Bus6,
+
+      /// <summary>
+      /// Восьмишинная структура стойки коммутации.
+      /// </summary>
+      Bus8,
+
+      /// <summary>
+      /// Комбинированное подключение к шинам.
+      /// </summary>
+      BusCombined,
+    }
+  }
+}
---
+++ Ask.Engine/ControlCommandAnalyser/Formatter/VshCommandFormatter.cs
@@ -0,0 +1,72 @@
+﻿using Ask.Core.Shared.Metadata.Enums.TranslationEnums;
+using Ask.Engine.ControlCommandAnalyser.Model;
+
+namespace Ask.Engine.ControlCommandAnalyser.Formatter
+{
+  public class VshCommandFormatter : ICommandFormatter
+  {
+    public bool CanFormat(BaseCommandModel model) => model is VshCommandModel;
+
+    public IEnumerable<string> Format(BaseCommandModel model)
+    {
+      if (model is not VshCommandModel vsh)
+        yield break;
+
+      var firstLine = $"{vsh.CommandNumber} {vsh.Mnemonic}";
+      yield return firstLine;
+
+      // Ключи
+      if (vsh.AlgorithmKey.Count > 0)
+        yield return $"\tКлючи команды: {string.Join(", ", vsh.AlgorithmKey)}";
+
+      if(vsh.BusStructure.Count == 0)
+      {
+        yield return $"\tСтруктура стойки коммутации не указана!";
+      }
+      else
+      {
+        yield return $"\tСтруктура стойки коммутации:";
+      }
+
+      foreach (var item in vsh.BusStructure)
+      {
+        if (item.Value.Count > 0)
+        {
+          if (item.Key == BusStructureEnum.Type.Bus2)
+          {
+            yield return $"\t\tДвухшинное подключение: {string.Join(", ", item.Value)}";
+          }
+          if (item.Key == BusStructureEnum.Type.Bus4)
+          {
+            yield return $"\t\tЧетырехшинное подключение: {string.Join(", ", item.Value)}";
+          }
+          if (item.Key == BusStructureEnum.Type.Bus6)
+          {
+            yield return $"\t\tШестишинное подключение: {string.Join(", ", item.Value)}";
+          }
+          if (item.Key == BusStructureEnum.Type.Bus8)
+          {
+            yield return $"\t\tВосьмишинное подключение: {string.Join(", ", item.Value)}";
+          }
+          if (item.Key == BusStructureEnum.Type.BusCombined)
+          {
+            yield return $"\t\tКомбинированное подключение: {string.Join(", ", item.Value)}";
+          }
+        }
+      }
+
+      if (vsh.Comment.Count > 0)
+      {
+        yield return $"\tКомметрии:";
+        foreach (var line in vsh.Comment)
+        {
+          var trimmed = line.Trim();
+          if (!string.IsNullOrEmpty(trimmed))
+            yield return $"\t\t{trimmed}";
+        }
+      }
+
+      yield return string.Empty;
+    }
+  }
+}
