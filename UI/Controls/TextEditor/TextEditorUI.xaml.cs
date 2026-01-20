fa121963ebba1d87b8206a8e5f81d16ffb23669a
Akfa4man <vanafomin7602@gmail.com>
2026-01-20 08:11:05 +0400

    Сделал так, чтобы прога не ломалась, когда код транслируется по нажатию F9.

--- UI/Controls/TextEditor/TextEditorUI.xaml.cs
+++ UI/Controls/TextEditor/TextEditorUI.xaml.cs
@@ -416,49 +416,50 @@ namespace UI.Controls.TextEditor
     public TextEditorUI(FileType fileType = FileType.None, TextEditorModel textEditorModel = null)
     {
       InitializeComponent();
+
       FileTypeDock = fileType;
       TextEditorModel = textEditorModel;
       _defaultFontSize = textEditor.FontSize;
 
       textEditor.PreviewKeyDown += TextEditor_PreviewKeyDown;
 
+      if (_executionMargin == null)
+      {
+        _executionMargin = new ExecutionGlyphMargin(textEditor);
+        textEditor.TextArea.LeftMargins.Insert(0, _executionMargin);
+      }
+
+      if (_markerService == null)
+      {
+        if (textEditor.Document == null)
+          textEditor.Document = new TextDocument();
+
+        _markerService = new TextMarkerService(textEditor);
+
+        var textView = textEditor.TextArea.TextView;
+        textView.BackgroundRenderers.Add(_markerService);
+
+        var services = textView.Services;
+        if (services.GetService(typeof(TextMarkerService)) == null)
+          services.AddService(typeof(TextMarkerService), _markerService);
+      }
+
       Loaded += (s, e) =>
       {
-        if (_markerService == null)
-        {
-          _markerService = new TextMarkerService(textEditor);
-          textEditor.TextArea.TextView.BackgroundRenderers.Add(_markerService);
-
-          var services = textEditor.TextArea.TextView.Services;
-          if (services.GetService(typeof(TextMarkerService)) == null)
-          {
-            services.AddService(typeof(TextMarkerService), _markerService);
-          }
-
-          Console.WriteLine("TextMarkerService зарегистрирован.");
-        }
-        else
-        {
-          Console.WriteLine("TextMarkerService уже инициализирован.");
-        }
-
         ApplySyntaxHighlighting(UserInterfaceConfig.GetSyntaxHighlighting());
-
-        if (_executionMargin == null)
-        {
-          _executionMargin = new ExecutionGlyphMargin(textEditor);
-          textEditor.TextArea.LeftMargins.Insert(0, _executionMargin);
-        }
-
       };
 
       HelpProvider.SetHelpKeyProvider(textEditor, () =>
       {
         var sel = textEditor.SelectedText?.Trim();
-        return string.IsNullOrWhiteSpace(sel) ? "DescriptionWorkTextEditor" : sel;
+        return string.IsNullOrWhiteSpace(sel)
+          ? "DescriptionWorkTextEditor"
+          : sel;
       });
 
-      EventAggregator.Subscribe<ThemeEvent.SyntaxHighlighting>(e => ApplySyntaxHighlighting(e.IsEnabled));
+      EventAggregator.Subscribe<ThemeEvent.SyntaxHighlighting>(
+        e => ApplySyntaxHighlighting(e.IsEnabled)
+      );
     }
 
     #endregion
