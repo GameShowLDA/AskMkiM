fda602cb5c745c4acb01e79347bb98075e6b9cd9
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2026-01-28 09:07:24 +0400

    Fir ERR2

--- Ask.Engine/ControlCommandExecutor/BaseStrategies/PairwiseFirstPointCheckerAlt.cs
+++ Ask.Engine/ControlCommandExecutor/BaseStrategies/PairwiseFirstPointCheckerAlt.cs
@@ -8,6 +8,7 @@ using Ask.Core.Shared.Metadata.Enums.TranslationEnums;
 using Ask.Core.Shared.Metadata.Static.Messages;
 using Ask.Engine.ControlCommandAnalyser.Model;
 using Ask.Engine.ControlCommandAnalyser.Model.Chains;
+using Ask.Engine.ControlCommandExecutor.BaseStrategies.Data;
 using Ask.Engine.ControlCommandExecutor.Execution;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,22 +22,23 @@ namespace Ask.Engine.ControlCommandExecutor.BaseStrategies
     /// <param name="points">Список точек для проверки.</param>
     /// <param name="messageService">Сервис отображения сообщений.</param>
     /// <returns>Задача, представляющая выполнение проверки.</returns>
-    static public async Task<List<ShowMessageModel>> CheckSequenceAsync(SchemeModel schemeModel, CommandExecutionManager manager, BaseCommandModel baseCommandModel, IUserInteractionService messageService, double resistance = 0, double cabelResistance = 0)
+    static public async Task<List<ShowMessageModel>> CheckSequenceAsync(PairwiseFirstPointAltContext context)
     {
       List<ShowMessageModel> errorsMessgae = new List<ShowMessageModel>();
+      var baseCommandModel = context.CommandModel;
 
-      List<List<ChainModel>> errorChain = new();
-      var pointsListSource = schemeModel.GetPointsConnected();
+      List <List<ChainModel>> errorChain = new();
+      var pointsListSource = context.SchemeModel.GetPointsConnected();
       if (pointsListSource.Count == 0)
       {
         return errorsMessgae;
       }
 
-      await messageService.ShowMessageAsync(ExecutorMessageBuilder.BuildCheckBlockHeader(ControlCheckAlgorithm.DisconnectionRelativeToFirstPoint));
+      await context.MessageService.ShowMessageAsync(ExecutorMessageBuilder.BuildCheckBlockHeader(ControlCheckAlgorithm.DisconnectionRelativeToFirstPoint));
 
       foreach (var groups in pointsListSource)
       {
-        messageService.GetCancellationToken().ThrowIfCancellationRequested();
+        context.MessageService.GetCancellationToken().ThrowIfCancellationRequested();
         foreach (var chains in groups.ChainModels)
         {
           bool errorPoint = false;
@@ -48,12 +50,12 @@ namespace Ask.Engine.ControlCommandExecutor.BaseStrategies
           }
           str = str.Remove(str.Length - 1);
 
-          await messageService.ShowMessageAsync(ExecutorMessageBuilder.BuildChainCheckBlock(str));
+          await context.MessageService.ShowMessageAsync(ExecutorMessageBuilder.BuildChainCheckBlock(str));
 
           var _basePoint = chains.PointModels.First();
-          await ConnectToBusAAndBAsync(messageService, _basePoint);
+          await ConnectToBusAAndBAsync(context.MessageService, _basePoint);
 
-          var Rt1 = await GetResistanceAsync(messageService, resistance);
+          var Rt1 = await GetResistanceAsync(context.MessageService, context.Value);
           if (Rt1 > 100)
           {
             var machineAdress = await DeviceDisplayConfig.GetMachineAddressVisibilityAsync() ? $"[{_basePoint.ToString()}]" : string.Empty;
@@ -61,29 +63,29 @@ namespace Ask.Engine.ControlCommandExecutor.BaseStrategies
             var errorMessageModels = new ShowMessageModel($"{_basePoint.Mnemonic}{machineAdress}", message: "Rизм = Нет подлючения точки", type: ShowMessageModel.MessageType.Error) { IndentLevel = 1 };
             errorPoint = true;
 
-            await messageService.ShowMessageAsync(errorMessageModels);
+            await context.MessageService.ShowMessageAsync(errorMessageModels);
             errorsMessgae.Add(errorMessageModels);
-            await messageService.ShowMessageAsync(new ShowMessageModel(debug: $"Добавлена ошибка: {errorMessageModels.ToString()}"));
-            manager.AddErrorMethod(EhtErrors.PointNotConnected($"{baseCommandModel.CommandNumber} {baseCommandModel.Mnemonic}", $"{_basePoint}{machineAdress}", messageService.GetLastLineNumber(), baseCommandModel.FormattedStartLineNumber));
+            await context.MessageService.ShowMessageAsync(new ShowMessageModel(debug: $"Добавлена ошибка: {errorMessageModels.ToString()}"));
+            context.CommandManager.AddErrorMethod(EhtErrors.PointNotConnected($"{baseCommandModel.CommandNumber} {baseCommandModel.Mnemonic}", $"{_basePoint}{machineAdress}", context.MessageService.GetLastLineNumber(), baseCommandModel.FormattedStartLineNumber));
           }
           else
           {
             var machineAdress = await DeviceDisplayConfig.GetMachineAddressVisibilityAsync() ? $"[{_basePoint.ToString()}]" : string.Empty;
             if (await DeviceDisplayConfig.GetMeasurementResultsVisibilityAsync())
             {
-              await messageService.ShowMessageAsync(new ShowMessageModel($"{_basePoint.Mnemonic}{machineAdress}", message: $"Rизм = {Rt1:F5} Ом", type: ShowMessageModel.MessageType.Success) { IndentLevel = 1 });
+              await context.MessageService.ShowMessageAsync(new ShowMessageModel($"{_basePoint.Mnemonic}{machineAdress}", message: $"Rизм = {Rt1:F5} Ом", type: ShowMessageModel.MessageType.Success) { IndentLevel = 1 });
             }
           }
 
-          await DeviceManager.DisconnectPointFromBusBAsync(_basePoint, messageService);
+          await DeviceManager.DisconnectPointFromBusBAsync(_basePoint, context.MessageService);
 
           for (int i = 1; i < chains.PointModels.Count; i++)
           {
-            messageService.GetCancellationToken().ThrowIfCancellationRequested();
+            context.MessageService.GetCancellationToken().ThrowIfCancellationRequested();
             var point = chains.PointModels[i];
-            await ConnectToBusAAndBAsync(messageService, point);
+            await ConnectToBusAAndBAsync(context.MessageService, point);
 
-            var Rt2 = await GetResistanceAsync(messageService, resistance);
+            var Rt2 = await GetResistanceAsync(context.MessageService, context.Value);
             if (Rt2 > 100)
             {
               var machineAdress = await DeviceDisplayConfig.GetMachineAddressVisibilityAsync() ? $"[{point.ToString()}]" : string.Empty;
@@ -91,21 +93,21 @@ namespace Ask.Engine.ControlCommandExecutor.BaseStrategies
               var errorMessageModels = new ShowMessageModel($"{point.Mnemonic}{machineAdress}", message: $"Нет подлючения точки", type: ShowMessageModel.MessageType.Error) { IndentLevel = 1 };
               errorPoint = true;
 
-              await messageService.ShowMessageAsync(errorMessageModels);
+              await context.MessageService.ShowMessageAsync(errorMessageModels);
               errorsMessgae.Add(errorMessageModels);
-              manager.AddErrorMethod(EhtErrors.PointNotConnected($"{baseCommandModel.CommandNumber} {baseCommandModel.Mnemonic}", $"{point.Mnemonic}{machineAdress}", messageService.GetLastLineNumber(), baseCommandModel.FormattedStartLineNumber));
-              await messageService.ShowMessageAsync(new ShowMessageModel(debug: $"Добавлена ошибка: {errorMessageModels.ToString()}"));
+              context.CommandManager.AddErrorMethod(EhtErrors.PointNotConnected($"{baseCommandModel.CommandNumber} {baseCommandModel.Mnemonic}", $"{point.Mnemonic}{machineAdress}", context.MessageService.GetLastLineNumber(), baseCommandModel.FormattedStartLineNumber));
+              await context.MessageService.ShowMessageAsync(new ShowMessageModel(debug: $"Добавлена ошибка: {errorMessageModels.ToString()}"));
             }
             else
             {
               var machineAdress = await DeviceDisplayConfig.GetMachineAddressVisibilityAsync() ? $"[{point.ToString()}]" : string.Empty;
               if (await DeviceDisplayConfig.GetMeasurementResultsVisibilityAsync())
               {
-                await messageService.ShowMessageAsync(new ShowMessageModel($"{point.Mnemonic}{machineAdress}", message: $"Rизм = {Rt2:F5} Ом", type: ShowMessageModel.MessageType.Success) { IndentLevel = 1 });
+                await context.MessageService.ShowMessageAsync(new ShowMessageModel($"{point.Mnemonic}{machineAdress}", message: $"Rизм = {Rt2:F5} Ом", type: ShowMessageModel.MessageType.Success) { IndentLevel = 1 });
               }
             }
 
-            await DeviceManager.DisconnectPointFromBusAAsync(point, messageService);
+            await DeviceManager.DisconnectPointFromBusAAsync(point, context.MessageService);
 
             double Rt = -1;
             var LowerBound = (baseCommandModel as EhtCommandModel).LowerLimitResistance.Value;
@@ -116,28 +118,30 @@ namespace Ask.Engine.ControlCommandExecutor.BaseStrategies
 
             if (!errorPoint)
             {
-              Rt = await GetResistanceAsync(messageService, resistance);
+              Rt = await GetResistanceAsync(context.MessageService, context.Value);
 
               if (Rt > 100)
               {
-                var errorMessageModels = new ShowMessageModel($"{_basePoint.Mnemonic}{machineAdressFirst}, {point.Mnemonic}{machineAdressSecond}", message: $"Overload", type: ShowMessageModel.MessageType.Error) { IndentLevel = 1 };
+                var errorMessageModels = ExecutorMessageBuilder.BuildMeasurementResultMessage(context.TypeCommand, context.LowerLimit, context.HigherLimit, Rt, chains: $"{_basePoint.Mnemonic}{machineAdressFirst}, {point.Mnemonic}{machineAdressSecond}", overload: true);
+                errorMessageModels.Status = ShowMessageModel.MessageType.Error;
+                errorMessageModels.IndentLevel = 1;
                 errorPoint = true;
 
-                await messageService.ShowMessageAsync(errorMessageModels);
-                manager.AddErrorMethod(EhtErrors.CircuitOverload($"{baseCommandModel.CommandNumber} {baseCommandModel.Mnemonic}", $"{_basePoint.Mnemonic}{machineAdressFirst}", $"{point.Mnemonic}{machineAdressSecond}", messageService.GetLastLineNumber(), baseCommandModel.FormattedStartLineNumber));
+                await context.MessageService.ShowMessageAsync(errorMessageModels);
+                context.CommandManager.AddErrorMethod(EhtErrors.CircuitOverload($"{baseCommandModel.CommandNumber} {baseCommandModel.Mnemonic}", $"{_basePoint.Mnemonic}{machineAdressFirst}", $"{point.Mnemonic}{machineAdressSecond}", context.MessageService.GetLastLineNumber(), baseCommandModel.FormattedStartLineNumber));
                 errorsMessgae.Add(errorMessageModels);
-                await messageService.ShowMessageAsync(new ShowMessageModel(debug: $"Добавлена ошибка: {errorMessageModels.ToString()}"));
+                await context.MessageService.ShowMessageAsync(new ShowMessageModel(debug: $"Добавлена ошибка: {errorMessageModels.ToString()}"));
               }
               else
               {
-                await messageService.ShowMessageAsync(new ShowMessageModel($"{_basePoint.Mnemonic}{machineAdressFirst}, {point.Mnemonic}{machineAdressSecond}", message: $"{Rt:F5} Ом") { IndentLevel = 1 });
+                await context.MessageService.ShowMessageAsync(new ShowMessageModel($"{_basePoint.Mnemonic}{machineAdressFirst}, {point.Mnemonic}{machineAdressSecond}", message: $"{Rt:F5} Ом") { IndentLevel = 1 });
               }
             }
 
-            await DeviceManager.DisconnectPointFromBusBAsync(point, messageService);
+            await DeviceManager.DisconnectPointFromBusBAsync(point, context.MessageService);
             if (!errorPoint)
             {
-              await messageService.ShowMessageAsync(new ShowMessageModel("Итог измерений"));
+              await context.MessageService.ShowMessageAsync(new ShowMessageModel("Итог измерений"));
 
               double Rx = 0;
               if (!errorPoint)
@@ -151,7 +155,7 @@ namespace Ask.Engine.ControlCommandExecutor.BaseStrategies
 
               var result = !await ExecutionConfig.GetIsIdleModeEnabled() ? Rx : !await ExecutionConfig.GetIsErrorSimulationEnabled() ? (LowerBound + UpperBound) / 2 : Rx;
 
-              result -= cabelResistance;
+              result -= context.CabelResistance;
 
               if (result < 0)
               {
@@ -166,19 +170,19 @@ namespace Ask.Engine.ControlCommandExecutor.BaseStrategies
                 type: succes ? ShowMessageModel.MessageType.Success : ShowMessageModel.MessageType.Error)
               { IndentLevel = 3 };
 
-              await messageService.ShowMessageAsync(error);
+              await context.MessageService.ShowMessageAsync(error);
 
               if (!succes)
               {
                 errorsMessgae.Add(error);
-                manager.AddErrorMethod(EhtErrors.ResistanceOutOfRange($"{baseCommandModel.CommandNumber} {baseCommandModel.Mnemonic}", result, _basePoint.ToString(), point.ToString(), LowerBound, UpperBound, messageService.GetLastLineNumber(), baseCommandModel.FormattedStartLineNumber));
+                context.CommandManager.AddErrorMethod(EhtErrors.ResistanceOutOfRange($"{baseCommandModel.CommandNumber} {baseCommandModel.Mnemonic}", result, _basePoint.ToString(), point.ToString(), LowerBound, UpperBound, context.MessageService.GetLastLineNumber(), baseCommandModel.FormattedStartLineNumber));
 
-                await messageService.ShowMessageAsync(new ShowMessageModel(debug: $"Добавлена ошибка: {error.ToString()}"));
+                await context.MessageService.ShowMessageAsync(new ShowMessageModel(debug: $"Добавлена ошибка: {error.ToString()}"));
               }
             }
           }
 
-          await DisconnectAllPoints(messageService, chains);
+          await DisconnectAllPoints(context.MessageService, chains);
         }
       }
 
