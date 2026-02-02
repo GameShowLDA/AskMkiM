e7cc511cedda2d6dda526b514f7c65a0449c12da
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2026-02-02 13:44:50 +0400

    Реализовал проверку питания МеШ

--- Ask.Core/Shared/Interfaces/DeviceInterfaces/Chassis/Capabilities/IPowerManagerChassis.cs
+++ Ask.Core/Shared/Interfaces/DeviceInterfaces/Chassis/Capabilities/IPowerManagerChassis.cs
@@ -19,5 +19,17 @@ namespace Ask.Core.Shared.Interfaces.DeviceInterfaces.Chassis.Capabilities
     /// </summary>
     /// <returns>Асинхронная задача.</returns>
     Task StartPowerAsync(IUserInteractionService? userMessageService = null);
+
+    /// <summary>
+    /// Асинхронно проверяет наличие и корректность питания системы.
+    /// </summary>
+    /// <param name="userMessageService">
+    /// Сервис пользовательского взаимодействия, используемый для отображения сообщений (опционально).
+    /// </param>
+    /// <returns>
+    /// <see langword="true"/>, если питание присутствует и соответствует требованиям;  
+    /// <see langword="false"/> — если питание отсутствует или некорректно.
+    /// </returns>
+    Task<bool> VerifyPowerAsync(IUserInteractionService? userMessageService = null);
   }
 }
