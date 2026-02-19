edbbab0243e2700ddca783683ceb86a0f73fec7f
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2026-02-19 16:42:33 +0400

    Анимация плюсика

--- Ask.UI/Shared/Components/Icons/PlusCircleIcon.xaml.cs
+++ Ask.UI/Shared/Components/Icons/PlusCircleIcon.xaml.cs
@@ -1,5 +1,6 @@
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace Ask.UI.Shared.Components.Icons
 {
@@ -12,6 +13,13 @@ namespace Ask.UI.Shared.Components.Icons
         typeof(PlusCircleIcon),
         new PropertyMetadata(16d));
 
+    public static readonly DependencyProperty IsMagnetizedProperty =
+      DependencyProperty.Register(
+        nameof(IsMagnetized),
+        typeof(bool),
+        typeof(PlusCircleIcon),
+        new PropertyMetadata(false, OnIsMagnetizedChanged));
+
     public PlusCircleIcon()
     {
       InitializeComponent();
@@ -21,6 +29,40 @@ namespace Ask.UI.Shared.Components.Icons
     {
       get => (double)GetValue(SizeProperty);
       set => SetValue(SizeProperty, value);
+    }
+
+    public bool IsMagnetized
+    {
+      get => (bool)GetValue(IsMagnetizedProperty);
+      set => SetValue(IsMagnetizedProperty, value);
+    }
+
+    private static void OnIsMagnetizedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      if (d is PlusCircleIcon control && e.NewValue is bool isMagnetized)
+      {
+        control.PlayMagnetAnimation(isMagnetized);
+      }
+    }
+
+    private void PlayMagnetAnimation(bool isMagnetized)
+    {
+      if (Resources["MagnetInStoryboard"] is not Storyboard magnetIn ||
+          Resources["MagnetOutStoryboard"] is not Storyboard magnetOut)
+      {
+        return;
+      }
+
+      if (isMagnetized)
+      {
+        magnetOut.Stop(this);
+        magnetIn.Begin(this, true);
+      }
+      else
+      {
+        magnetIn.Stop(this);
+        magnetOut.Begin(this, true);
+      }
     }
   }
 }
