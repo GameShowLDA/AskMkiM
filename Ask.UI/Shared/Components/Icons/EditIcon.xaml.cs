6c502d1d564e40fc309c3662430b51f4850a4d2c
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2026-02-19 16:35:57 +0400

    Анимация карандашика в конфигурации

--- Ask.UI/Shared/Components/Icons/EditIcon.xaml.cs
+++ Ask.UI/Shared/Components/Icons/EditIcon.xaml.cs
@@ -1,5 +1,6 @@
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace Ask.UI.Shared.Components.Icons
 {
@@ -12,6 +13,13 @@ namespace Ask.UI.Shared.Components.Icons
         typeof(EditIcon),
         new PropertyMetadata(16d));
 
+    public static readonly DependencyProperty IsWritingProperty =
+      DependencyProperty.Register(
+        nameof(IsWriting),
+        typeof(bool),
+        typeof(EditIcon),
+        new PropertyMetadata(false, OnIsWritingChanged));
+
     public EditIcon()
     {
       InitializeComponent();
@@ -21,6 +29,40 @@ namespace Ask.UI.Shared.Components.Icons
     {
       get => (double)GetValue(SizeProperty);
       set => SetValue(SizeProperty, value);
+    }
+
+    public bool IsWriting
+    {
+      get => (bool)GetValue(IsWritingProperty);
+      set => SetValue(IsWritingProperty, value);
+    }
+
+    private static void OnIsWritingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      if (d is EditIcon control && e.NewValue is bool isWriting)
+      {
+        control.AnimateWriting(isWriting);
+      }
+    }
+
+    private void AnimateWriting(bool isWriting)
+    {
+      if (Resources["WriteStartStoryboard"] is not Storyboard writeStart ||
+          Resources["WriteStopStoryboard"] is not Storyboard writeStop)
+      {
+        return;
+      }
+
+      if (isWriting)
+      {
+        writeStop.Stop(this);
+        writeStart.Begin(this, true);
+      }
+      else
+      {
+        writeStart.Stop(this);
+        writeStop.Begin(this, true);
+      }
     }
   }
 }
