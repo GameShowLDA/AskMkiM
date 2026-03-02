35384a43a6dad45e9f74b7c7876d33d71850615b
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2026-03-02 10:16:07 +0400

    Новая кнопка печати

---
+++ Ask.UI/Shared/Components/Icons/PrintConfigIcon.xaml.cs
@@ -0,0 +1,68 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Ask.UI.Shared.Components.Icons
+{
+  public partial class PrintConfigIcon : UserControl
+  {
+    public static readonly DependencyProperty SizeProperty =
+      DependencyProperty.Register(
+        nameof(Size),
+        typeof(double),
+        typeof(PrintConfigIcon),
+        new PropertyMetadata(16d));
+
+    public static readonly DependencyProperty IsPrintingProperty =
+      DependencyProperty.Register(
+        nameof(IsPrinting),
+        typeof(bool),
+        typeof(PrintConfigIcon),
+        new PropertyMetadata(false, OnIsPrintingChanged));
+
+    public PrintConfigIcon()
+    {
+      InitializeComponent();
+    }
+
+    public double Size
+    {
+      get => (double)GetValue(SizeProperty);
+      set => SetValue(SizeProperty, value);
+    }
+
+    public bool IsPrinting
+    {
+      get => (bool)GetValue(IsPrintingProperty);
+      set => SetValue(IsPrintingProperty, value);
+    }
+
+    private static void OnIsPrintingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      if (d is PrintConfigIcon control && e.NewValue is bool isPrinting)
+      {
+        control.PlayPrintAnimation(isPrinting);
+      }
+    }
+
+    private void PlayPrintAnimation(bool isPrinting)
+    {
+      if (Resources["PrintInStoryboard"] is not Storyboard printIn ||
+          Resources["PrintOutStoryboard"] is not Storyboard printOut)
+      {
+        return;
+      }
+
+      if (isPrinting)
+      {
+        printOut.Stop(this);
+        printIn.Begin(this, true);
+      }
+      else
+      {
+        printIn.Stop(this);
+        printOut.Begin(this, true);
+      }
+    }
+  }
+}
