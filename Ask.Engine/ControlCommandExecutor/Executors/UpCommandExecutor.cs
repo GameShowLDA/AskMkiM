900cfc73d0ef198afb6a61f2b330fbc7d4e9aa95
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2026-03-04 15:39:33 +0400

    Починил УП и ЦУ

--- Ask.Engine/ControlCommandExecutor/Executors/UpCommandExecutor.cs
+++ Ask.Engine/ControlCommandExecutor/Executors/UpCommandExecutor.cs
@@ -1,6 +1,7 @@
 ﻿using Ask.Core.Services.Extensions;
 using Ask.Core.Shared.DTO.Protocol;
 using Ask.Core.Shared.Metadata.Enums.TranslationEnums.Commands;
+using Ask.Core.Shared.Metadata.Static.Messages;
 using Ask.Engine.ControlCommandAnalyser;
 using Ask.Engine.ControlCommandAnalyser.Model;
 using Ask.Engine.ControlCommandExecutor.Execution;
@@ -15,13 +16,20 @@ namespace Ask.Engine.ControlCommandExecutor.Executors
     public async Task ExecuteAsync(CommandExecutionContext context, ProtocolModel protocolModel)
     {
       var command = GetRequiredCommand<UpCommandModel>(context);
+      SetActiveLine(context, command);
 
-      if (CommandExecutionState.LastCuResult == MessageBoxResult.No)
+      var nameCommand = $"{command.CommandNumber} {command.Mnemonic}";
+      var message = BuildSourceLinesMessage(command);
+
+      await context.Console.ShowMessageAsync(ExecutorMessageBuilder.BuildCommandExecutionMessage(nameCommand, message), IsBlockStart: true);
+
+      if (CommandExecutionState.LastRejectFlag)
       {
         context.JumpToCommandNumber?.Invoke(command.TargetLabel);
       }
 
       CommandExecutionState.LastCuResult = MessageBoxResult.None;
+      CommandExecutionState.LastRejectFlag = false;
     }
   }
 }
