4b377e1ac35917161b6c54782b264df99d9b00fa
JuliaZorina <96383396+JuliaZorina@users.noreply.github.com>
2026-03-24 13:06:44 +0400

    подправила датагрид

--- UI/Controls/TextEditor/TextEditorContainer.xaml.cs
+++ UI/Controls/TextEditor/TextEditorContainer.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Ask.Core.Shared.Interfaces.ExecutionInterfaces;
+using Ask.Core.Services.EventCore.Adapters;
+using Ask.Core.Shared.Interfaces.ExecutionInterfaces;
 using System.Windows.Controls;
 using UI.Windows.WpfDocking.Windows.Docking;
 
@@ -9,9 +10,19 @@ namespace UI.Controls.TextEditor
   /// </summary>
   public partial class TextEditorContainer : UserControl, ITextAdapter
   {
+    private bool _dockEventsAttached;
+
     public TextEditorContainer()
     {
       InitializeComponent();
+      Loaded += (_, _) => AttachDockManagerEvents();
+      IsVisibleChanged += (_, _) =>
+      {
+        if (IsVisible)
+        {
+          SyncActiveEditorState();
+        }
+      };
     }
 
     public string GetText()
@@ -66,12 +77,61 @@ namespace UI.Controls.TextEditor
 
       if (dockItem != null)
       {
-        dockItem.Close(); // закрывает и удаляет из DockManager
+        dockItem.Close();
         return true;
       }
 
       return false;
     }
 
+    public void SyncActiveEditorState()
+    {
+      if (!IsLoaded || !IsVisible)
+      {
+        return;
+      }
+
+      var activeDockItem = DockManager.DockItems
+        .FirstOrDefault(item => item.IsActiveDocument || item.IsActiveItem);
+
+      if (activeDockItem?.Content is TextEditorUI textEditor)
+      {
+        EditorEventAdapter.RaiseTextEditorActive(true);
+        EditorEventAdapter.RaiseTextEditorActivated(textEditor);
+        return;
+      }
+
+      if (activeDockItem?.Content is TranslatorItem translatorItem)
+      {
+        var leftEditor = translatorItem.GetLeftBox()?.GetTextEditor();
+        if (leftEditor != null)
+        {
+          EditorEventAdapter.RaiseTextEditorActive(true);
+          EditorEventAdapter.RaiseTextEditorActivated(leftEditor);
+          return;
+        }
+      }
+
+      EditorEventAdapter.RaiseTextEditorActive(false);
+    }
+
+    private void AttachDockManagerEvents()
+    {
+      if (_dockEventsAttached)
+      {
+        return;
+      }
+
+      DockManager.ActiveItemChanged += DockManager_ActiveItemChanged;
+      DockManager.ActiveDocumentChanged += DockManager_ActiveItemChanged;
+      _dockEventsAttached = true;
+
+      SyncActiveEditorState();
+    }
+
+    private void DockManager_ActiveItemChanged(object? sender, EventArgs e)
+    {
+      SyncActiveEditorState();
+    }
   }
 }
