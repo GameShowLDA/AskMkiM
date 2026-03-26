79997ac068f7b8e6da34409d76a286c217a0d766
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2026-03-26 19:05:18 +0400

    Инициализация новой БД

--- Ask.DataBase.Provider/Context/AppDbContext.cs
+++ Ask.DataBase.Provider/Context/AppDbContext.cs
@@ -1,35 +1,32 @@
-﻿using Ask.DataBase.Provider.Configuration;
+using Ask.DataBase.Provider.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
-namespace Ask.DataBase.Provider.Context
+namespace Ask.DataBase.Provider.Context;
+
+/// <summary>
+/// Контекст базы данных для управления устройствами.
+/// </summary>
+public partial class AppDbContext : DbContext
 {
+  public AppDbContext() { }
+
+  public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
   /// <summary>
-  /// Контекст базы данных для управления устройствами.
+  /// Конфигурация базы данных.
   /// </summary>
-  public partial class AppDbContext : DbContext
+  /// <param name="optionsBuilder">
+  /// Построитель параметров контекста базы данных. Используется для задания параметров подключения.
+  /// </param>
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
   {
-    public AppDbContext() { }
+    optionsBuilder.ConfigureWarnings(warnings =>
+      warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
 
-    /// <summary>
-    /// Конфигурация базы данных.
-    /// </summary>
-    /// <param name="optionsBuilder">
-    /// Построитель параметров контекста базы данных. Используется для задания параметров подключения.
-    /// </param>
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    if (!optionsBuilder.IsConfigured)
     {
-      if (optionsBuilder.IsConfigured == false)
-      {
-        optionsBuilder.UseSqlite($"Data Source={DbPathResolver.Resolve()}");
-      }
+      optionsBuilder.UseSqlite($"Data Source={DbPathResolver.Resolve()}");
     }
-
-    /// <summary>
-    /// Инициализирует новый экземпляр контекста базы данных <see cref="AppDbContext"/>.
-    /// </summary>
-    /// <param name="options">
-    /// Параметры конфигурации контекста, включая источник данных и поведение подключения.
-    /// </param>
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
   }
 }
