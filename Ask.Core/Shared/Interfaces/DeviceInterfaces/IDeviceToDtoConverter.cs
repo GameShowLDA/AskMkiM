6a5cfeeb9345f6501016e2b4a9998d248c425173
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2026-03-28 09:45:56 +0400

    отправил файлики

---
+++ Ask.Core/Shared/Interfaces/DeviceInterfaces/IDeviceToDtoConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ask.Core.Shared.Interfaces.DeviceInterfaces
+{
+  /// <summary>
+  /// Определяет контракт для типобезопасного преобразования устройства в DTO.
+  /// </summary>
+  /// <typeparam name="TDevice">Тип устройства.</typeparam>
+  /// <typeparam name="TDto">Тип DTO.</typeparam>
+  public interface IDeviceToDtoConverter<out TDto>
+  {
+    /// <summary>
+    /// Преобразует устройство в DTO.
+    /// </summary>
+    /// <param name="device">Экземпляр устройства.</param>
+    /// <returns>DTO, соответствующий устройству.</returns>
+    TDto Convert();
+  }
+}
