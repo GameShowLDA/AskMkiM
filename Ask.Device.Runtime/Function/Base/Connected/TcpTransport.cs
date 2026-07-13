a3794237bed0b9cf944c0273a2f58984ce036126
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2026-07-13 18:15:43 +0400

    Подправил сообщение об ошибке подключения к системе

--- Ask.Device.Runtime/Function/Base/Connected/TcpTransport.cs
+++ Ask.Device.Runtime/Function/Base/Connected/TcpTransport.cs
@@ -45,7 +45,7 @@ namespace Ask.Device.Runtime.Function.Connected
         }
       }
 
-      return (false, $"Нет подключения к {_device.Name}");
+      return (false, $"Нет связи с {_device.Name}");
     }
 
     /// <inheritdoc />
